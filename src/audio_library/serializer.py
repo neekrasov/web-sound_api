036347b0ef791fb92030b7294bab4bec11ec911a
neekrasov <neekrasov@gmail.com>
2022-04-25 00:42:16 +0300

    add serializers by comment and fix any moments

--- src/audio_library/serializer.py
+++ src/audio_library/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+
 from . import models
 from ..oauth.base.services import delete_old_file
+from ..oauth.serializer import AuthorSerializer
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -26,12 +28,10 @@ class AlbumSerializer(serializers.ModelSerializer):
 
 
 class CreateAuthorTrackSerializer(serializers.ModelSerializer):
-    plays_count = serializers.IntegerField(read_only=True)
-    downoad_count = serializers.IntegerField(read_only=True)
-
     class Meta:
         model = models.Track
         fields = (
+            'user',
             'id',
             'title',
             'license',
@@ -39,13 +39,21 @@ class CreateAuthorTrackSerializer(serializers.ModelSerializer):
             'album',
             'link_of_download',
             'file_to_download',
+            'private',
+            'cover',
             'created_at',
             'plays_count',
             'downoad_count',
         )
+        read_only_fields = (
+            'plays_count',
+            'downoad_count',
+            'user',
+        )
 
     def update(self, instance, validated_data):
         delete_old_file(instance.file_to_download.path)
+        delete_old_file(instance.cover.path)
         return super().update(instance, validated_data)
 
 
@@ -53,6 +61,7 @@ class TrackSerializer(CreateAuthorTrackSerializer):
     license = LicenseSerializer()
     genre = GenreSerializer(many=True)
     album = AlbumSerializer()
+    user = AuthorSerializer()
 
 
 class CreatePlayListSerializer(serializers.ModelSerializer):
@@ -67,3 +76,18 @@ class CreatePlayListSerializer(serializers.ModelSerializer):
 
 class PlayListSerializer(CreatePlayListSerializer):
     tracks = TrackSerializer(many=True, read_only=True)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = AuthorSerializer()
+
+    class Meta:
+        model = models.Comment
+        fields = ('id', 'text', 'user', 'track', 'created_at')
+
+
+class CommentAuthorSerializer(serializers.ModelSerializer):
+    """ Сериализация для CRUD комментариев автором """
+    class Meta:
+        model = models.Comment
+        fields = ('id', 'text', 'track')
